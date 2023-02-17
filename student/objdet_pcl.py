fce0b0f64fdd9da24eaeb7b098969f6ffc397995
Duc-Nhan Nguyen <ducnhan.nguyen89@gmail.com>
2023-02-18 00:49:48 +0100

    implement ID_S2_EX2

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -291,12 +291,18 @@ def bev_from_pcl(lidar_pcl, configs):
     print("student task ID_S2_EX1")
 
     ## step 1 :  compute bev-map discretization by dividing x-range by the bev-image height (see configs)
+    dX = (configs.lim_x[1] - configs.lim_x[0]) / configs.bev_height
+    dY = (configs.lim_y[1] - configs.lim_y[0]) / configs.bev_width
 
-    ## step 2 : create a copy of the lidar pcl and transform all metrix x-coordinates into bev-image coordinates    
+    ## step 2 : create a copy of the lidar pcl and transform all matrix x-coordinates into bev-image coordinates
+    lidarCpy = lidar_pcl.copy()
+    lidarCpy[:, 0] = np.int_(np.floor(lidarCpy[:, 0] / dX))
 
     # step 3 : perform the same operation as in step 2 for the y-coordinates but make sure that no negative bev-coordinates occur
+    lidarCpy[:, 1] = np.int_(np.floor(lidarCpy[:, 1] / dY) + (configs.bev_width + 1) / 2)
 
     # step 4 : visualize point-cloud using the function show_pcl from a previous task
+    show_pcl(lidarCpy)
     
     #######
     ####### ID_S2_EX1 END #######     
